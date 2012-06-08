41cd68f02607f3c5b755ab35d27283899a3f1feb
starkingwx <starkingwx@gmail.com>
2012-06-08 18:49:51 +0800

    Initial Commit

---
+++ ectoolkit/ectoolkit.h
@@ -0,0 +1,13 @@
+//
+//  ectoolkit.h
+//  ectoolkit
+//
+//  Created by star king on 12-6-8.
+//  Copyright (c) 2012年 elegant cloud. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ectoolkit : NSObject
+
+@end
---
+++ ectoolkit/ectoolkit.m
@@ -0,0 +1,13 @@
+//
+//  ectoolkit.m
+//  ectoolkit
+//
+//  Created by star king on 12-6-8.
+//  Copyright (c) 2012年 elegant cloud. All rights reserved.
+//
+
+#import "ectoolkit.h"
+
+@implementation ectoolkit
+
+@end
